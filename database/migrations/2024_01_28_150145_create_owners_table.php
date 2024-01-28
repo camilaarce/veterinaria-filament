4dd3694d3604a450598c19781c1b7f3539db4079
camilaarce <camilarce2710@gmail.com>
2024-01-28 12:06:45 -0300

    migracion owners

--- database/migrations/2024_01_28_150145_create_owners_table.php
+++ database/migrations/2024_01_28_150145_create_owners_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('owners', function (Blueprint $table) {
             $table->id();
+            $tabl->string('email');
+            $tabl->string('name');
+            $tabl->string('phone');
             $table->timestamps();
         });
     }
